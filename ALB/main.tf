89ca7f44b40f77fad1394c5fd9d57c5300672217
adrianUala <adri.eze.martinez@gmail.com>
2020-04-21 11:04:55 -0300

    add files for ALB with SG

---
+++ ALB/main.tf
@@ -0,0 +1,132 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_lb_target_group" "target-group" {
+  health_check {
+    interval            = 10
+    path                = "/"
+    protocol            = "HTTP"
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+
+  name        = "test-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "instance"
+  vpc_id      = "${aws_vpc.default.id}"
+}
+
+resource "aws_lb" "aws-alb" {
+  name     = "test-alb"
+  internal = false
+
+  security_groups = [
+    "${aws_security_group.alb-sg.id}",
+  ]
+
+  subnets = [
+    "${aws_subnet.default.id}",
+    "${aws_subnet.default.id}",
+  ]
+
+  tags = {
+    Name = "test-alb"
+  }
+
+  ip_address_type    = "ipv4"
+  load_balancer_type = "application"
+}
+
+resource "aws_lb_listener" "test-alb-listner" {
+  load_balancer_arn = "${aws_lb.aws-alb.arn}"
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.target-group.arn}"
+  }
+}
+
+resource "aws_security_group" "alb-sg" {
+  name   = "alb-sg"
+  vpc_id = "${aws_vpc.default.id}"
+}
+
+resource "aws_security_group_rule" "inbound_ssh" {
+  from_port         = 22
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.alb-sg.id}"
+  to_port           = 22
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "inbound_http" {
+  from_port         = 80
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.alb-sg.id}"
+  to_port           = 80
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "outbound_all" {
+  from_port         = 0
+  protocol          = "-1"
+  security_group_id = "${aws_security_group.alb-sg.id}"
+  to_port           = 0
+  type              = "egress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "default" {
+  vpc_id = "${aws_vpc.default.id}"
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = "${aws_vpc.default.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.default.id}"
+}
+
+
+resource "aws_subnet" "default" {
+  vpc_id                  = "${aws_vpc.default.id}"
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_security_group" "tf_sg" {
+  name        = "terraform_sg"
+  description = "Grupo de seguridad"
+  vpc_id      = "${aws_vpc.default.id}"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # “-1” significa TCP y UDP
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
